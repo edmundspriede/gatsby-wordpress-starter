f9c51fbbca23e72984e0cb57def9e9ea2072fda2
Edmunds Priede <edmunds@kls.lv>
2024-12-30 13:32:58 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,7 +28,7 @@ module.exports = {
         // the only required plugin option for WordPress is the GraphQL url.
         url:
           process.env.WPGRAPHQL_URL ||
-          `https://simabove.com/graphql`,
+          `https://anahata.vq1kgoofc0-yjr3om7n061m.p.temp-site.link/graphql`,
       },
         schema: {
           timeout: 1200000,
